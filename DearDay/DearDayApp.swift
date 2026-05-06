c8a78a8f00987c0b2b189bbd0145718a5d5606cb
JaeWoong Eum <wodnd0418@gmail.com>
2026-05-06 09:03:06 +0900

    Initial Commit

---
+++ DearDay/DearDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  DearDayApp.swift
+//  DearDay
+//
+//  Created by wodnd on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct DearDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
